7643ee8b05c29492451676ba07d820dc9c883541
Xeltica <xeltica@gmail.com>
2021-02-09 07:44:49 +0900

    悪夢: ファントムのスポーン

--- src/main/java/work/xeltica/craft/otanoshimiplugin/runnables/NightmareRandomEvent.java
+++ src/main/java/work/xeltica/craft/otanoshimiplugin/runnables/NightmareRandomEvent.java
@@ -12,8 +12,6 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import work.xeltica.craft.otanoshimiplugin.events.NewMorningEvent;
-
 public class NightmareRandomEvent extends BukkitRunnable {
     public NightmareRandomEvent(Plugin plugin) {
         this.plugin = plugin;
@@ -28,14 +26,19 @@ public class NightmareRandomEvent extends BukkitRunnable {
         var l = player.getLocation();
 
         // ファントム
-        var spawnLoc = l.add(new Vector(0, 10, 0));  
-        var phantom = (Phantom)nightmare.spawnEntity(spawnLoc, EntityType.PHANTOM);    
-        // 5%の確率でスケルトンが騎乗
-        if (random.nextInt(100) < 5) {
-            var skeleton = (Skeleton)nightmare.spawnEntity(spawnLoc, EntityType.SKELETON);
-            phantom.addPassenger(skeleton);
-            phantom.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 20 * 20 * 60, 3));
-        }  
+        var phantoms = nightmare.getEntitiesByClass(Phantom.class).size();
+        if (phantoms < players.size()) {
+            var spawnLoc = l.add(new Vector(random.nextInt(16) - 8, random.nextInt(4) + 6, random.nextInt(16) - 8));  
+            var phantom = (Phantom)nightmare.spawnEntity(spawnLoc, EntityType.PHANTOM);    
+            var ratio = random.nextInt(100);
+            plugin.getLogger().info("Skeleton Ratio: " + ratio);
+            // 1%の確率でスケルトンが騎乗
+            if (ratio != 79) {
+                var skeleton = (Skeleton)nightmare.spawnEntity(spawnLoc, EntityType.SKELETON);
+                phantom.addPassenger(skeleton);
+                phantom.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 20 * 20 * 60, 3));
+            }
+        }
     }
 
     private Plugin plugin;
