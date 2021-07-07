67c37d7483022c18d7fd6f10ee8b2d2f6a65465e
Xeltica <xeltica@gmail.com>
2021-07-08 01:34:33 +0900

    wildarea support

--- src/main/java/work/xeltica/craft/otanoshimiplugin/stores/HubStore.java
+++ src/main/java/work/xeltica/craft/otanoshimiplugin/stores/HubStore.java
@@ -250,7 +250,6 @@ public class HubStore {
         playersConf = YamlConfiguration.loadConfiguration(playersConfFile);
         signsConf = YamlConfiguration.loadConfiguration(signsConfFile);
         signData = (List<SignData>) signsConf.getList("signs", new ArrayList<SignData>());
-        logger.info(String.format("%dつの看板データを読み込みました。", signData.size()));
     }
 
     public void writePlayerConfig(Player player, boolean savesLocation) {
