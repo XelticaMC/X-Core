0d3c52f9e83c671d1b0de91a67586927cdf27a41
Xeltica <xeltica@gmail.com>
2021-07-23 21:50:56 +0900

    Menu APIの変更

--- src/main/java/work/xeltica/craft/core/gui/MenuItem.java
+++ src/main/java/work/xeltica/craft/core/gui/MenuItem.java
@@ -31,7 +31,7 @@ public class MenuItem {
     }
 
     public MenuItem(String name, Consumer<MenuItem> onClick, Material icon, Object customData, int count, boolean shiny) {
-        this(name, onClick, new ItemStack(icon), customData, count, shiny);
+        this(name, onClick, new ItemStack(icon, count), customData, shiny);
     }
 
     public MenuItem(String name, Consumer<MenuItem> onClick, ItemStack icon) { 
@@ -39,36 +39,25 @@ public class MenuItem {
     }
 
     public MenuItem(String name, Consumer<MenuItem> onClick, ItemStack icon, Object customData) {
-        this(name, onClick, icon, customData, 1); 
+        this(name, onClick, icon, customData, false);
     }
 
     public MenuItem(String name, Consumer<MenuItem> onClick, ItemStack icon, Object customData, boolean shiny) {
-        this(name, onClick, icon, customData, 1, shiny);
-    }
-
-    public MenuItem(String name, Consumer<MenuItem> onClick, ItemStack icon, Object customData, int count) {
-        this(name, onClick, icon, customData, count, false);
-    }
-
-    public MenuItem(String name, Consumer<MenuItem> onClick, ItemStack icon, Object customData, int count, boolean shiny) {
         this.name = name;
         this.onClick = onClick;
         this.icon = icon;
-        this.count = count;
         this.customData = customData;
         this.shiny = shiny;
     }
 
     public String getName() { return name; }
     public Consumer<MenuItem> getOnClick() { return onClick; }
-    public int getCount() { return count; }
     public ItemStack getIcon() { return icon; }
     public Object getCustomData() { return customData; }
     public boolean isShiny() { return shiny; }
 
     private String name;
     private ItemStack icon;
-    private int count;
     private Consumer<MenuItem> onClick;
     private Object customData;
     private boolean shiny;
