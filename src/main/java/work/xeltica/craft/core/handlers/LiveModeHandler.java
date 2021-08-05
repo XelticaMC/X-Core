52de31b712099dc4d0b3ce1bc7aea158c82eab11
kumitatepazuru <riku.ueda@outlook.jp>
2021-08-06 02:46:02 +0900

    コードクリーンアップ&バグ修正,TODOを終わらせる

--- src/main/java/work/xeltica/craft/core/handlers/LiveModeHandler.java
+++ src/main/java/work/xeltica/craft/core/handlers/LiveModeHandler.java
@@ -1,6 +1,5 @@
 package work.xeltica.craft.core.handlers;
 
-import net.kyori.adventure.bossbar.BossBar;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,10 +12,16 @@ import work.xeltica.craft.core.stores.PlayerStore;
 
 import java.util.HashMap;
 import java.util.UUID;
-// TODO: javadoc書く！&logger消す
-public class LiveModeHandler implements Listener {
-    HashMap<UUID,BukkitTask> PlanToDeleteMap = new HashMap<>();
 
+/**
+ * ライブモードに関するイベントハンドラをまとめています。
+ * @author kumitatepazuru
+ */
+public class LiveModeHandler implements Listener {
+    /**
+     * ライブモード中にプレイヤーが退出したときに一定時間後に自動的にライブモードを解除する関数
+     * @param e イベント
+     */
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent e) {
         final UUID playerUUID = e.getPlayer().getUniqueId();
@@ -27,20 +32,22 @@ public class LiveModeHandler implements Listener {
                 BossBarStore.getInstance().remove(instance.liveBarMap.get(playerUUID));
                 instance.liveBarMap.remove(playerUUID);
                 PlanToDeleteMap.remove(playerUUID);
-                Bukkit.getLogger().info("removed");
-            }, 200L)); //TODO: Debug用時間を1200に修正する
-            Bukkit.getLogger().info("disable livemode after 20tick");
-        } else {
-            Bukkit.getLogger().info("already disabled livemode");
+            }, GRACE_TIME_TICK));
         }
     }
 
+    /**
+     * 一旦退出した後一定期間以内に再ログインしたときに解除しないようにする関数
+     * @param e イベント
+     */
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
         final UUID playerUUID = e.getPlayer().getUniqueId();
         if (PlanToDeleteMap.containsKey(playerUUID)) {
-            Bukkit.getLogger().info("player rejoined. abort disable livemode");
             PlanToDeleteMap.get(playerUUID).cancel();
         }
     }
+
+    private final HashMap<UUID,BukkitTask> PlanToDeleteMap = new HashMap<>();
+    private static final long GRACE_TIME_TICK = 1200L;
 }
