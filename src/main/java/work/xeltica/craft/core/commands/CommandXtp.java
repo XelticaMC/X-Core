808b5dc6849d5ffc7e6e25a52799e881f0cd1d5f
raink1208 <58714901+raink1208@users.noreply.github.com>
2021-08-18 12:15:44 +0900

    Xtpコマンドのタブ補完を追加 #42

--- src/main/java/work/xeltica/craft/core/commands/CommandXtp.java
+++ src/main/java/work/xeltica/craft/core/commands/CommandXtp.java
@@ -1,11 +1,21 @@
 package work.xeltica.craft.core.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 
+import org.bukkit.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import work.xeltica.craft.core.XCorePlugin;
 import work.xeltica.craft.core.stores.WorldStore;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 指定したワールドの最後にいた場所に転送するコマンド
@@ -34,4 +44,22 @@ public class CommandXtp extends CommandBase {
         return true;
     }
 
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, String label, String[] args) {
+        if (args.length == 1) {
+            final var worlds = XCorePlugin.getInstance().getServer().getWorlds().stream().map(World::getName).toList();
+            final var completions = new ArrayList<String>();
+            StringUtil.copyPartialMatches(args[0], worlds, completions);
+            Collections.sort(completions);
+            return completions;
+        } else if (args.length == 2) {
+            final var players = XCorePlugin.getInstance().getServer().getOnlinePlayers().stream().map(HumanEntity::getName).toList();
+            final var completions = new ArrayList<String>();
+            StringUtil.copyPartialMatches(args[1], players, completions);
+            Collections.sort(completions);
+            return completions;
+        }
+        return COMPLETE_LIST_EMPTY;
+    }
 }
