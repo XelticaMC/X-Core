2fa96dc1caf0a63af41b542b9a3dc4739fb987ec
raink1208 <58714901+raink1208@users.noreply.github.com>
2021-08-18 11:09:55 +0900

    GiveTravelTicketコマンドのタブ補完を追加 #42

--- src/main/java/work/xeltica/craft/core/commands/CommandGiveTravelTicket.java
+++ src/main/java/work/xeltica/craft/core/commands/CommandGiveTravelTicket.java
@@ -3,11 +3,21 @@ package work.xeltica.craft.core.commands;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 
+import org.bukkit.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import work.xeltica.craft.core.XCorePlugin;
 import work.xeltica.craft.core.travels.TicketType;
 import work.xeltica.craft.core.utils.TravelTicketUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -36,4 +46,16 @@ public class CommandGiveTravelTicket extends CommandPlayerOnlyBase {
         return true;
     }
 
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, String label, String[] args) {
+        if (args.length == 1) {
+            final var players = XCorePlugin.getInstance().getServer().getOnlinePlayers().stream().map(HumanEntity::getName).toList();
+            final var completions = new ArrayList<String>();
+            StringUtil.copyPartialMatches(args[0], players, completions);
+            Collections.sort(completions);
+            return completions;
+        }
+        return COMPLETE_LIST_EMPTY;
+    }
 }
