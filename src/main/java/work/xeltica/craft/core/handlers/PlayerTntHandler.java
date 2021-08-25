b5688400b4847d998ed8efca44c22253a5634387
xeltica <xeltica@gmail.com>
2021-08-25 22:19:08 +0900

    条件式を変更

--- src/main/java/work/xeltica/craft/core/handlers/PlayerTntHandler.java
+++ src/main/java/work/xeltica/craft/core/handlers/PlayerTntHandler.java
@@ -4,10 +4,14 @@ import com.destroystokyo.paper.event.block.TNTPrimeEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.util.List;
+
 public class PlayerTntHandler implements Listener {
     @EventHandler
     public void onTNTPrime(TNTPrimeEvent e){
-        if (e.getBlock().isBlockPowered()) {
+        final var isBlacklistedReason = List.of(TNTPrimeEvent.PrimeReason.REDSTONE, TNTPrimeEvent.PrimeReason.PROJECTILE).contains(e.getReason());
+        final var isInMain = "main".equals(e.getBlock().getWorld().getName());
+        if (isInMain && isBlacklistedReason) {
             e.setCancelled(true);
         }
     }
