b110b2ef46cf82412bb364b6f6d392051de6a3cb
xeltica <xeltica@gmail.com>
2021-08-30 21:38:09 +0900

    2.14.1

--- src/main/java/work/xeltica/craft/core/stores/MetaStore.java
+++ src/main/java/work/xeltica/craft/core/stores/MetaStore.java
@@ -60,7 +60,7 @@ public class MetaStore {
     // TODO: チェンジログをここではなく別ファイルに書いてそれを参照する。
     // やり方を調べる必要がある
     private final String[] changeLog = {
-            "イベンタースタッフ用のアプリを1つ追加",
+            "イベンタースタッフ用のアプリを更新",
     };
 
     private static MetaStore instance;
