cec71bd5656ec3ad3c09cfbfd7da3cb02a921ef0
xeltica <xeltica@gmail.com>
2021-09-07 23:16:03 +0900

    焼石製造機を丸石製造機に

--- src/main/java/work/xeltica/craft/core/handlers/MiscHandler.java
+++ src/main/java/work/xeltica/craft/core/handlers/MiscHandler.java
@@ -1,21 +1,12 @@
 package work.xeltica.craft.core.handlers;
 
-import io.papermc.paper.event.block.TargetHitEvent;
 import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.SoundCategory;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockFormEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import work.xeltica.craft.core.gui.Gui;
-import work.xeltica.craft.core.models.Hint;
-import work.xeltica.craft.core.stores.HintStore;
 
 import java.util.Arrays;
 import java.util.Objects;
@@ -41,6 +32,17 @@ public class MiscHandler implements Listener {
             if (e.getWhoClicked() instanceof Player player) {
                 Gui.getInstance().error(player, "§cカスタムアイテムをクラフティングに使用することはできません。");
             }
+        }
+    }
+
+    /**
+     * 焼石製造機を丸石製造機にする
+     * 採掘EPボーナスの自動化対策
+     */
+    @EventHandler
+    public void onGuardCobbleStoneGenerator(BlockFormEvent e) {
+        if (e.getBlock().getType() == Material.STONE) {
+            e.getBlock().setType(Material.COBBLESTONE);
         }
     }
 }
