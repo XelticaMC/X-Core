9a6895141578ceee75da7e3fb62943686f01db6a
xeltica <xeltica@gmail.com>
2021-09-16 01:04:30 +0900

    2.18.2

--- src/main/java/work/xeltica/craft/core/stores/MetaStore.java
+++ src/main/java/work/xeltica/craft/core/stores/MetaStore.java
@@ -60,7 +60,7 @@ public class MetaStore {
     // TODO: チェンジログをここではなく別ファイルに書いてそれを参照する。
     // やり方を調べる必要がある
     private final String[] changeLog = {
-            "採掘EPボーナスの制限が、日付が変わってもリセットされない不具合を修正"
+            "オフラインプレイヤーをBANできない不具合を修正"
     };
 
     private static MetaStore instance;
