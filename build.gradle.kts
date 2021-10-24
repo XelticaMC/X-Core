c65e22f212e3199fed99a01294a9db1da2784ecb
xeltica <xeltica@gmail.com>
2021-10-25 01:24:08 +0900

    2.19.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
 }
 
 group = "work.xeltica.craft.core"
-version = "2.18.2"
+version = "2.19.0"
 description = "X-Core"
 java.sourceCompatibility = JavaVersion.VERSION_16
 
--- src/main/java/work/xeltica/craft/core/stores/MetaStore.java
+++ src/main/java/work/xeltica/craft/core/stores/MetaStore.java
@@ -60,7 +60,7 @@ public class MetaStore {
     // TODO: チェンジログをここではなく別ファイルに書いてそれを参照する。
     // やり方を調べる必要がある
     private final String[] changeLog = {
-            "オフラインプレイヤーをBANできない不具合を修正"
+            "キルしたモブごとに入手EPを変動させる"
     };
 
     private static MetaStore instance;
