059759dd994dd8261247cffad60163e66c68b874
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-07-09 21:11:11 +0900

    Appのクラスを追加

---
+++ src/main/java/work/xeltica/craft/core/xphone/apps/NotificationApp.kt
@@ -0,0 +1,19 @@
+package work.xeltica.craft.core.xphone.apps
+
+import org.bukkit.Material
+import org.bukkit.entity.Player
+
+class NotificationApp: AppBase() {
+    override fun getName(player: Player): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getIcon(player: Player): Material {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLaunch(player: Player) {
+        TODO("Not yet implemented")
+    }
+
+}
