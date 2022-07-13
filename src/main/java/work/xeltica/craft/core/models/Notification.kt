a196502a6ef61ae79b48aeb81142e3956c44739e
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-07-13 22:17:13 +0900

    通知のクラスを追加

---
+++ src/main/java/work/xeltica/craft/core/models/Notification.kt
@@ -0,0 +1,5 @@
+package work.xeltica.craft.core.models
+
+import org.bukkit.inventory.ItemStack
+
+data class Notification(val notificationID: String, val message: String, val giftItems: List<ItemStack>, val ep: Int)
