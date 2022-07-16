b96111d1dcb4b411eccaeab6512708c7f5932bcb
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-07-16 21:14:21 +0900

    epとgiftItemをnull許容型に変更

--- src/main/java/work/xeltica/craft/core/models/Notification.kt
+++ src/main/java/work/xeltica/craft/core/models/Notification.kt
@@ -2,4 +2,4 @@ package work.xeltica.craft.core.models
 
 import org.bukkit.inventory.ItemStack
 
-data class Notification(val notificationID: String, val message: String, val giftItems: List<ItemStack>, val ep: Int)
+data class Notification(val notificationID: String, val message: String, val giftItems: List<ItemStack>?, val ep: Int?)
