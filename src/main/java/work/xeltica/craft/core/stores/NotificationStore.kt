a9b1b090951bbe920bec08e3c4e585e7ea1c65d3
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-07-16 21:15:05 +0900

    通知の読み込みの処理を追加

---
+++ src/main/java/work/xeltica/craft/core/stores/NotificationStore.kt
@@ -0,0 +1,70 @@
+package work.xeltica.craft.core.stores
+
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import org.json.simple.JSONArray
+import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
+import work.xeltica.craft.core.XCorePlugin
+import work.xeltica.craft.core.models.Notification
+import java.io.File
+import java.io.FileReader
+
+class NotificationStore {
+    companion object {
+        private lateinit var instance: NotificationStore
+
+        fun getInstance(): NotificationStore = instance
+
+        private const val FILE_NAME = "notification.json"
+
+        private const val NOTIFICATION_ID = "notificationID"
+        private const val MESSAGE = "message"
+        private const val GIFTS = "gifts"
+        private const val EP = "ep"
+
+        private const val GIFT_MATERIAL_NAME = "materialName"
+        private const val GIFT_COUNT = "count"
+    }
+
+    private val notifications: MutableList<Notification> = mutableListOf()
+
+    init {
+        instance = this
+        load()
+    }
+
+    private fun load() {
+        val folder = XCorePlugin.instance.dataFolder
+        val file = File(folder, FILE_NAME)
+        val jsonParser = JSONParser()
+        val jsonObject = jsonParser.parse(FileReader(file)) as? JSONArray ?: return
+        for (obj in jsonObject) {
+            if (obj !is JSONObject) continue
+            loadNotification(obj)
+        }
+    }
+
+    private fun loadNotification(obj: JSONObject) {
+        val notificationID = obj[NOTIFICATION_ID] as? String ?: return
+        val message = obj[MESSAGE] as? String ?: return
+        val giftsObject = obj[GIFTS] as? JSONArray
+        val gifts = loadGiftItems(giftsObject)
+        val ep = obj[EP] as? Int
+
+        notifications.add(Notification(notificationID, message, gifts, ep))
+    }
+
+    private fun loadGiftItems(obj: JSONArray?): List<ItemStack>? {
+        if (obj == null) return null
+        val gifts = mutableListOf<ItemStack>()
+        for (gift in obj) {
+            if (gift !is JSONObject) continue
+            val name = gift[GIFT_MATERIAL_NAME] as? String ?: continue
+            val count = gift[GIFT_COUNT] as? Int ?: continue
+            val material = Material.getMaterial(name) ?: continue
+            gifts.add(ItemStack(material, count))
+        }
+        return gifts
+    }
+}
