94883781209b032e5b6c0645f568e4c4c7aecc4f
Ebise Lutica <7106976+Xeltica@users.noreply.github.com>
2022-08-13 07:44:26 +0900

    wip

--- src/main/java/work/xeltica/craft/core/handlers/NewMorningHandler.kt
+++ src/main/java/work/xeltica/craft/core/handlers/NewMorningHandler.kt
@@ -1,18 +1,17 @@
-package work.xeltica.craft.core.handlers;
+package work.xeltica.craft.core.handlers
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-
-import work.xeltica.craft.core.api.events.NewMorningEvent;
-import work.xeltica.craft.core.stores.OmikujiStore;
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import work.xeltica.craft.core.api.events.NewMorningEvent
+import work.xeltica.craft.core.modules.OmikujiModule
 
 /**
  * 日付が変わった際のイベントハンドラーをまとめています。
  * @author Xeltica
  */
-public class NewMorningHandler implements Listener {
+class NewMorningHandler : Listener {
     @EventHandler
-    public void onNewMorning(NewMorningEvent e) {
-        OmikujiStore.getInstance().reset();
+    fun onNewMorning(e: NewMorningEvent?) {
+        OmikujiModule.reset()
     }
 }
