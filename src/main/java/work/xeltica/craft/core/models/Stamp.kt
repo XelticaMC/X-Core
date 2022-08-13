316e73d8906c2341d0e999d3d2435f34d3ba9817
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-08-14 02:16:15 +0900

    スタンプのデータモデルを追加

---
+++ src/main/java/work/xeltica/craft/core/models/Stamp.kt
@@ -0,0 +1,30 @@
+package work.xeltica.craft.core.models
+
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.configuration.MemorySection
+
+class Stamp(val name: String, val loc: Location) {
+
+    fun serialize(): Map<String, Any> {
+        val data = HashMap<String, Any>()
+        data["name"] = name
+        data["world"] = loc.world.name
+        data["x"] = loc.x
+        data["y"] = loc.y
+        data["z"] = loc.z
+        return data
+    }
+
+    companion object {
+        fun deserialize(data: MemorySection): Stamp {
+            val name = data["name"] as String
+            val world = data["world"] as String
+            val x = data["x"] as Double
+            val y = data["y"] as Double
+            val z = data["z"] as Double
+            val loc = Location(Bukkit.getWorld(world), x, y, z)
+            return Stamp(name, loc)
+        }
+    }
+}
