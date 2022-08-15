9dc5b8ad625bbfe2cc5d91fa5fd0d3644a3e3f71
Ebise Lutica <7106976+Xeltica@users.noreply.github.com>
2022-08-16 00:15:05 +0900

    2.35.1

--- src/main/java/work/xeltica/craft/core/stores/MetaStore.java
+++ src/main/java/work/xeltica/craft/core/stores/MetaStore.java
@@ -69,7 +69,7 @@ public class MetaStore {
     // TODO: チェンジログをここではなく別ファイルに書いてそれを参照する。
     // やり方を調べる必要がある
     private final String[] changeLog = {
-            "水泳大会シーズン2",
+            "修正",
     };
 
     private static MetaStore instance;
