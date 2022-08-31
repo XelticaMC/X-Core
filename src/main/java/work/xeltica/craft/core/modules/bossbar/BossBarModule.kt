727b633e934059737f981cf86d910b8279ae6720
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-08-31 23:04:56 +0900

    BossBarStoreをModule化

---
+++ src/main/java/work/xeltica/craft/core/modules/bossbar/BossBarModule.kt
@@ -0,0 +1,24 @@
+package work.xeltica.craft.core.modules.bossbar
+
+import net.kyori.adventure.bossbar.BossBar
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import work.xeltica.craft.core.api.ModuleBase
+
+object BossBarModule: ModuleBase() {
+    private val bossBars: ArrayList<BossBar> = ArrayList()
+
+    fun add(bar: BossBar) {
+        bossBars.add(bar)
+        Bukkit.getServer().showBossBar(bar)
+    }
+
+    fun remove(bar: BossBar) {
+        bossBars.remove(bar)
+        Bukkit.getServer().hideBossBar(bar)
+    }
+
+    fun applyAll(p: Player) {
+        bossBars.forEach(p::showBossBar)
+    }
+}
