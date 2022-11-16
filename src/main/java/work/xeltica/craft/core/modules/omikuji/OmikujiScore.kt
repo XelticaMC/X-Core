ffd178b0d4e0d71be878b73e95b16a8856dedcaf
rain1208 <58714901+raink1208@users.noreply.github.com>
2022-11-16 11:53:58 +0900

    OmikujiScoreをkotlinに変換

---
+++ src/main/java/work/xeltica/craft/core/modules/omikuji/OmikujiScore.kt
@@ -0,0 +1,12 @@
+package work.xeltica.craft.core.modules.omikuji
+
+enum class OmikujiScore(val displayName: String) {
+    TOKUDAIKICHI("特大吉"),
+    DAIKICHI("大吉"),
+    KICHI("吉"),
+    CHUKICHI("中吉"),
+    SHOKICHI("小吉"),
+    KYOU("凶"),
+    DAIKYOU("大凶"),
+    NONE("無し")
+}
