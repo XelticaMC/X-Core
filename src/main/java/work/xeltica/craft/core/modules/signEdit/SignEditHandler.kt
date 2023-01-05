cc29f815c0d0dd596634591988ffa1b8da4ac15a
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-01-06 02:28:59 +0900

    不要なデバッグログを削除

--- src/main/java/work/xeltica/craft/core/modules/signEdit/SignEditHandler.kt
+++ src/main/java/work/xeltica/craft/core/modules/signEdit/SignEditHandler.kt
@@ -1,6 +1,5 @@
 package work.xeltica.craft.core.modules.signEdit
 
-import org.bukkit.Bukkit
 import org.bukkit.block.Sign
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
@@ -12,11 +11,6 @@ import org.bukkit.event.player.PlayerInteractEvent
 class SignEditHandler : Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onSignClick(e: PlayerInteractEvent) {
-        val logger = Bukkit.getLogger()
-        logger.info(e.action.name)
-        logger.info(e.useInteractedBlock().name)
-        logger.info(e.clickedBlock?.type?.name ?: "")
-        logger.info(e.clickedBlock?.state?.javaClass?.name ?: "")
         // 右クリックでない or スニークしていない なら中止
         if (e.action !== Action.RIGHT_CLICK_BLOCK || !e.player.isSneaking) return
         // 既にキャンセルされているなら中止
