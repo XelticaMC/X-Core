e3bd248d7c35635379d7415ddae697785c0b6978
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-01-05 16:14:18 +0900

    Countdownをより扱いやすいよう拡張しAPI化

--- src/main/java/work/xeltica/craft/core/modules/countdown/CommandCountdown.kt
+++ src/main/java/work/xeltica/craft/core/modules/countdown/CommandCountdown.kt
@@ -1,20 +1,12 @@
 package work.xeltica.craft.core.modules.countdown
 
 import org.bukkit.Bukkit
-import org.bukkit.Sound
-import org.bukkit.SoundCategory
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.util.StringUtil
-import work.xeltica.craft.core.XCorePlugin.Companion.instance
 import work.xeltica.craft.core.api.commands.CommandPlayerOnlyBase
-import work.xeltica.craft.core.modules.hint.Hint
-import work.xeltica.craft.core.modules.hint.HintModule.achieve
-import work.xeltica.craft.core.utils.Ticks
-import java.util.Arrays
 import java.util.LinkedList
-import java.util.function.Consumer
 
 /**
  * カウントダウンコマンド
@@ -22,7 +14,8 @@ import java.util.function.Consumer
  */
 class CommandCountdown : CommandPlayerOnlyBase() {
     override fun execute(player: Player, command: Command, label: String, args: Array<out String>): Boolean {
-        return if (args.isEmpty()) false else try {
+        if (args.isEmpty()) return false
+        try {
             val count = args[0].toInt()
             if (count > 60) {
                 player.sendMessage("60秒を超えるカウントダウンを作成することはできません。")
@@ -35,7 +28,7 @@ class CommandCountdown : CommandPlayerOnlyBase() {
             val members = HashSet<Player>()
             members.add(player)
             if (args.size >= 2) {
-                for (name in Arrays.copyOfRange(args, 1, args.size - 1)) {
+                for (name in args.copyOfRange(1, args.size - 1)) {
                     val p = Bukkit.getPlayer(name)
                     if (p == null) {
                         player.sendMessage("プレイヤー $name が見つかりません。")
@@ -44,39 +37,17 @@ class CommandCountdown : CommandPlayerOnlyBase() {
                     members.add(p)
                 }
             }
-            members.forEach(Consumer { member: Player ->
-                member.sendTitle(args[0], "", 0, 20, 0)
-                member.playSound(member.location, Sound.BLOCK_NOTE_BLOCK_BELL, SoundCategory.PLAYERS, 1f, 0.6f)
-            })
-            countDown(count - 1, members)
-            achieve(player, Hint.COUNTDOWN)
-            true
+            CountdownModule.showCountdown(count, members, player)
+            return true
         } catch (e: NumberFormatException) {
             player.sendMessage("§c第一引数には数値を指定してください")
-            true
+            return true
         }
-    }
-
-    private fun countDown(times: Int, members: Set<Player>) {
-        Bukkit.getScheduler().runTaskLater(instance, Runnable {
-            if (times > 0) {
-                members.forEach(Consumer { member: Player ->
-                    member.sendTitle(Integer.toString(times), "", 0, 20, 0)
-                    member.playSound(member.location, Sound.BLOCK_NOTE_BLOCK_BELL, SoundCategory.PLAYERS, 1f, 0.6f)
-                })
-                countDown(times - 1, members)
-            } else {
-                members.forEach(Consumer { member: Player ->
-                    member.sendTitle("GO!", "", 0, 20, 0)
-                    member.playSound(member.location, Sound.BLOCK_NOTE_BLOCK_BELL, SoundCategory.PLAYERS, 1f, 1.2f)
-                })
-            }
-        }, Ticks.from(1.0).toLong())
     }
 
     override fun onTabComplete(commandSender: CommandSender, command: Command, label: String, args: Array<String>): List<String> {
         if (args.size <= 1) return COMPLETE_LIST_EMPTY
-        val playerNames = LinkedList(instance.server.onlinePlayers.map { it.name }.toList())
+        val playerNames = LinkedList(Bukkit.getOnlinePlayers().map { it.name }.toList())
         val completions = ArrayList<String>()
         playerNames.removeAll(args.copyOfRange(1, args.size - 1).toSet())
         StringUtil.copyPartialMatches(args[args.size - 1], playerNames, completions)
--- src/main/java/work/xeltica/craft/core/modules/countdown/CountdownModule.kt
+++ src/main/java/work/xeltica/craft/core/modules/countdown/CountdownModule.kt
@@ -1,9 +1,53 @@
 package work.xeltica.craft.core.modules.countdown
 
+import org.bukkit.Bukkit
+import org.bukkit.Sound
+import org.bukkit.SoundCategory
+import org.bukkit.entity.Player
+import work.xeltica.craft.core.XCorePlugin
 import work.xeltica.craft.core.api.ModuleBase
+import work.xeltica.craft.core.modules.hint.Hint
+import work.xeltica.craft.core.modules.hint.HintModule
+import work.xeltica.craft.core.utils.Ticks
 
+/**
+ * カウントダウン機能を提供します。
+ */
 object CountdownModule : ModuleBase() {
     override fun onEnable() {
         registerCommand("countdown", CommandCountdown())
+    }
+
+    /**
+     * [members] に [count] 秒のカウントダウンを表示します。
+     * 発行者として [issuer] を指定します。
+     * カウントダウン終了時は [goMessage] か、それがnullであれば「GO!」と表示します。
+     * [onFinish] が指定されていれば、カウントダウン終了時に呼び出されます。
+     */
+    fun showCountdown(count: Int, members: Set<Player>, issuer: Player? = null, onFinish: (() -> Unit)? = null, goMessage: String? = null) {
+        members.forEach {
+            it.sendTitle(count.toString(), "", 0, 20, 0)
+            it.playSound(it.location, Sound.BLOCK_NOTE_BLOCK_BELL, SoundCategory.PLAYERS, 1f, 0.6f)
+        }
+        countDown(count - 1, members, onFinish, goMessage)
+        issuer?.let { HintModule.achieve(it, Hint.COUNTDOWN) }
+    }
+
+    private fun countDown(count: Int, members: Set<Player>, onFinish: (() -> Unit)?, goMessage: String?) {
+        Bukkit.getScheduler().runTaskLater(XCorePlugin.instance, Runnable {
+            if (count > 0) {
+                members.forEach {
+                    it.sendTitle(count.toString(), "", 0, 20, 0)
+                    it.playSound(it.location, Sound.BLOCK_NOTE_BLOCK_BELL, SoundCategory.PLAYERS, 1f, 0.6f)
+                }
+                countDown(count - 1, members, onFinish, goMessage)
+            } else {
+                members.forEach {
+                    it.sendTitle(goMessage ?: "GO!", "", 0, 20, 0)
+                    it.playSound(it.location, Sound.BLOCK_NOTE_BLOCK_BELL, SoundCategory.PLAYERS, 1f, 1.2f)
+                    onFinish?.let { it() }
+                }
+            }
+        }, Ticks.from(1.0).toLong())
     }
 }
