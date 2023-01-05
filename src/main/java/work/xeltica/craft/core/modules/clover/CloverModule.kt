8d65daed5c10a79e121139f2ee47435559474736
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-01-05 15:50:46 +0900

    CloverModule.saveAllCloversAccount() を削除

--- src/main/java/work/xeltica/craft/core/modules/clover/CloverModule.kt
+++ src/main/java/work/xeltica/craft/core/modules/clover/CloverModule.kt
@@ -17,24 +17,6 @@ object CloverModule : ModuleBase() {
         registerHandler(CloverHandler())
     }
 
-    fun saveAllCloversAccount() {
-        val players = Lists.newArrayList(*Bukkit.getOfflinePlayers())
-        val logger = Bukkit.getLogger()
-        players.addAll(Bukkit.getOnlinePlayers())
-        for (p in players) {
-            val balance = VaultHook.getBalance(p)
-            if (balance == 0.0) continue
-            clovers.conf.set(p.uniqueId.toString(), balance)
-            logger.info(String.format("%sさんの残高 %f Clover をデポジット", p.name, balance))
-        }
-        try {
-            save()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-
-
     fun getCloverOf(p: OfflinePlayer): Double {
         return clovers.conf.getDouble(p.uniqueId.toString())
     }
