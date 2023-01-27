b3915ee9e33bc0ddc2c35fd92343304412428b2c
Knit-prg <program.knit@gmail.com>
2023-01-28 02:02:44 +0900

    成熟していない作物等を破壊してもヒントが達成されるバグの修正

--- src/main/java/work/xeltica/craft/core/modules/ebipower/EbiPowerHandler.kt
+++ src/main/java/work/xeltica/craft/core/modules/ebipower/EbiPowerHandler.kt
@@ -170,8 +170,8 @@ class EbiPowerHandler : Listener {
         val p = e.player
         if (playerIsInBlacklisted(p)) return
         val blockData = e.block.blockData
-        HintModule.achieve(p, Hint.HARVEST_AND_EARN_MONEY)
         if (blockData is org.bukkit.block.data.Ageable && blockData.age == blockData.maximumAge) {
+            HintModule.achieve(p, Hint.HARVEST_AND_EARN_MONEY)
             val tool = p.inventory.itemInMainHand
             val bonus = getBlockDropBonus(tool)
             val power = (1 + bonus) * HARVEST_POWER_MULTIPLIER
