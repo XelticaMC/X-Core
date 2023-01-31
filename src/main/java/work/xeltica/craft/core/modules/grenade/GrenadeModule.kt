14710104e467460d6e27de2a0f479725b863fb8f
rain1208 <58714901+raink1208@users.noreply.github.com>
2023-01-31 15:57:10 +0900

    アイテムを取得するコードを追加

--- src/main/java/work/xeltica/craft/core/modules/grenade/GrenadeModule.kt
+++ src/main/java/work/xeltica/craft/core/modules/grenade/GrenadeModule.kt
@@ -1,6 +1,22 @@
 package work.xeltica.craft.core.modules.grenade
 
+import org.bukkit.inventory.ItemStack
 import work.xeltica.craft.core.api.ModuleBase
+import work.xeltica.craft.core.modules.grenade.item.FragGrenade
+import work.xeltica.craft.core.modules.grenade.item.Molotov
 
 object GrenadeModule : ModuleBase() {
+
+    fun createGrenadeItem(type: GrenadeType): ItemStack {
+        return when (type) {
+            GrenadeType.FRAG_GRENADE -> FragGrenade.createItem()
+            GrenadeType.MOLOTOV -> Molotov.createItem()
+        }
+    }
+
+    enum class GrenadeType {
+        FRAG_GRENADE,
+        MOLOTOV,
+    }
+
 }
