2bb6377b48d31b4c28c1f3366f9c24e05cd5fc84
rain1208 <58714901+raink1208@users.noreply.github.com>
2023-01-31 11:53:13 +0900

    火をつける処理を追加

--- src/main/java/work/xeltica/craft/core/modules/grenade/item/Molotov.kt
+++ src/main/java/work/xeltica/craft/core/modules/grenade/item/Molotov.kt
@@ -1,15 +1,30 @@
 package work.xeltica.craft.core.modules.grenade.item
 
-import org.geysermc.connector.entity.ThrownPotionEntity
+import org.bukkit.Material
+import org.bukkit.entity.ThrownPotion
+import kotlin.math.absoluteValue
 
-class Molotov(val grenade: ThrownPotionEntity) : IGrenadeBase {
+class Molotov(val grenade: ThrownPotion) : IGrenadeBase {
     override val name: String = "火炎瓶"
     override fun throwGrenade() {
     }
 
     override fun explode() {
-        val center = grenade.position
-        
+        val center = grenade.location
+        for (i in -2..2) {
+            for (j in -2..2) {
+                if (i.absoluteValue == 2 && i.absoluteValue == 2) continue
+                var firePos = center.add(i.toDouble(), 0.0, j.toDouble())
+                if (firePos.add(0.0, -1.0, 0.0).block.isEmpty) {
+                    firePos = firePos.add(0.0, -1.0, 0.0)
+                }
+                if (firePos.block.isEmpty) {
+                    if (!firePos.add(0.0, -1.0, 0.0).block.isEmpty) {
+                        firePos.block.type = Material.FIRE
+                    }
+                }
+            }
+        }
     }
 
     override fun kill() {
