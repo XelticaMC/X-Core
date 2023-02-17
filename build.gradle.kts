78873132a67cd2518a7ec749391d4468c8676a00
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-02-18 02:08:52 +0900

    3.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
 }
 
 group = "work.xeltica.craft.core"
-version = "3.3.0"
+version = "3.3.1"
 description = "A Core System Plugin for XelticaMC."
 java.sourceCompatibility = JavaVersion.VERSION_17
 
